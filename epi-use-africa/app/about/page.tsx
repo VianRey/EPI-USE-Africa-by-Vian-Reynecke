9f054d34a9db666fa11ae26f40b0ee0d82128150
VianRey <vianreynecke@gmail.com>
2024-09-04 14:54:19 +0200

    Added styling for about page

--- epi-use-africa/app/about/page.tsx
+++ epi-use-africa/app/about/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React from "react";
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody, Button } from "@nextui-org/react";
 import CustomNavbar from "../components/navbar";
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 export default function AboutPage() {
   return (
@@ -15,11 +18,9 @@ export default function AboutPage() {
           transition={{ duration: 0.6 }}
           className="w-full max-w-[800px] mb-8"
         >
-          <Card className="bg-gradient-to-r from-purple-300 to-pink-300 dark:from-purple-800 dark:to-gray-600 rounded-xl shadow-lg">
+          <Card className="dark:bg-gray-700 bg-gray-100 rounded-xl shadow-lg">
             <CardBody>
-              <h2 className="text-3xl font-bold text-center mb-6 text-gray-900 dark:text-gray-100">
-                About Me
-              </h2>
+              <h2 className="text-3xl font-bold text-center mb-6">About Me</h2>
               <div className="text-center">
                 <motion.img
                   src="https://1.gravatar.com/avatar/781dd086d20f254c9ad1cf197f8797cdb7e8660f1e9af50133234e664c47dc6d?size=256"
@@ -50,6 +51,44 @@ export default function AboutPage() {
             </CardBody>
           </Card>
         </motion.div>
+        <div className="flex justify-center gap-6">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.4 }}
+          >
+            <Button href="https://github.com/VianRey" className="bg-orange-800">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+              <span className="text-lg">GitHub</span>
+            </Button>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.6 }}
+          >
+            <Button
+              href="https://www.linkedin.com/in/vian-reynecke-a80604282/"
+              className="bg-blue-600"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              <span className="text-lg">LinkedIn</span>
+            </Button>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.8 }}
+          >
+            <Button
+              href="https://blendweb.co.za"
+              className="dark:bg-white dark:text-gray-800 bg-gray-800 text-white"
+            >
+              <FontAwesomeIcon icon={faGlobe} size="2x" />
+              <span className="text-lg">BlendWeb</span>
+            </Button>
+          </motion.div>
+        </div>
       </div>
     </>
   );
